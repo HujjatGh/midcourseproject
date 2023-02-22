f2399b236fb8ad2580df80d5d881e7fc73fc62c4
HujjatGh <hujjat.pg@gmail.com>
2023-02-22 20:22:46 +0100

    basic changes

--- script.js
+++ script.js
@@ -1,1 +1,100 @@
-
+[
+    {
+      thumbnail: '001.png',
+      id:'N°01' ,
+      name: 'bulbasaur',
+      type: ['plant', 'poison'],
+    },
+    {
+      thumbnail: '002.png',
+      id:'N°02',
+      name: 'charmander',
+      type: ['fire'],
+    },
+    {
+      thumbnail: '003.png',
+      id:'N°03',
+      name: 'caterpie',
+      type: ['bug'],
+    },
+    {
+      thumbnail: '004.png',
+      id:'N°04',
+      name: 'kaluna',
+      type: ['bug', 'poison'],
+    },
+    {
+      thumbnail: '005.png',
+      id:'N°05',
+      name: 'ratata',
+      type: ['plant'],
+    },
+    {
+      thumbnail: '006.png',
+      id:'N°06',
+      name: 'pikachu',
+      type: ['normal'],
+    },
+    {
+      thumbnail: '007.png',
+      id:'N°07',
+      name: 'vulpix',
+      type: ['fire', 'plant'],
+    },
+    {
+      thumbnail: '008.png',
+      id:'N°08',
+      name: 'jigglypuff',
+      type: ['normal', 'fairy'],
+    },
+    {
+      thumbnail: '009.png',
+      id:'N°09',
+      name: 'ice',
+      type: ['bug'],
+    },
+    {
+      thumbnail: '010.png',
+      id:'N°10',
+      name: 'ground',
+      type: ['electrical'],
+    },
+    {
+      thumbnail: '011.png',
+      id:'N°11',
+      name: 'ghost',
+      type: ['poison'],
+    },
+    {
+      thumbnail: '012.png',
+      id:'N°12',
+      name: 'deer',
+      type: ['fire'],
+    },
+    {
+      thumbnail: '013.png',
+      id:'N°13',
+      name: 'dark',
+      type: ['fairy'],
+    },
+    {
+      thumbnail: '014.png',
+      id:'N°14',
+      name: 'stell',
+      type: ['normal', 'electrical'],
+    },
+    {
+      thumbnail: '015.png',
+      id:'N°15',
+      name: 'cat',
+      type: ['fire'],
+    },
+    {
+      thumbnail: '016.png',
+      id:'N°16',
+      name: 'rock',
+      type: ['fairy', 'poison'],
+    }
+  ]
+  
+  
